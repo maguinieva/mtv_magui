9dd5fa1f89a1c8f33070b69f86c6f6a5569dd6a5
мααgα ทi૯√α <maguii2000@gmail.com>
2022-10-22 20:29:25 -0300

    Creando plantillas

--- mtv/family/admin.py
+++ mtv/family/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from family.models import Family_member
+
+admin.site.register(Family_member)
--- mtv/family/views.py
+++ mtv/family/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from family.models import Family_member
+
+
+def fun_create_family_member(request, name: str, last_name: str, age: int, relationship: str):
+
+    member = Family_member(name=name, last_name=last_name, age=age, relationship=relationship)
+    member.save()
+
+    context_dict = {'member': member}
+    return render(
+        request=request,
+        context=context_dict,
+        template_name='family/create_family_member.html',
+    )
+
+def fun_show_family_members(request):
+    
+    members = Family_member.objects.all()
+
+    context_dict = {'members': members}
+
+    return render(
+        request=request,
+        context=context_dict,
+        template_name='family/list_family_members.html',
+    )
